1263213f7f6c87d0c752dd6f9c35d6ed10084f81
Dante Souza e Souza <lokinmodar@gmail.com>
2017-11-27 23:37:12 -0200

    Testing Observable to print List<String> data
    Using lambdas to print a List of Names for learning purposes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,28 @@
 import io.reactivex.*;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class HelloWorld {
     public static void main(String[] args) {
         Flowable.just("Hello world").subscribe(System.out::println);
+
+
+        List<String> names = new ArrayList<>();
+        names.add("Dante");
+        names.add("Delano");
+        names.add("Derek");
+        names.add("Dimitri");
+
+        Observable<String> stringObservable = Observable.fromIterable(names);
+
+        stringObservable.subscribe(System.out::println);
+
     }
+
+
+
+
 }
